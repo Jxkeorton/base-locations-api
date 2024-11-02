2c499bc6bd1cb9ebdb515518df0b8b356b95d620
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-11-02 23:24:26 +0000

    Update requirements

--- base_locations_api/settings.py
+++ base_locations_api/settings.py
@@ -58,7 +58,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), '127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), '127.0.0.1', 'localhost']
 
 
 # Application definition
