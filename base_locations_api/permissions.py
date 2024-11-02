d601bf2195cdcac4a1e1bca88b657a9325a5921b
Jxkeorton <jakeorton99@gmail.com>
2024-11-02 15:46:59 +0000

    Add locations views and urls , Add new permission to only allow superuser to modify

--- base_locations_api/permissions.py
+++ base_locations_api/permissions.py
@@ -6,3 +6,16 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user 
+
+
+class IsSuperUserOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only superusers to modify the data.
+    """
+
+    def has_permission(self, request, view):
+        # Allow read-only access for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Allow only superusers to make changes
+        return request.user.is_superuser
--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from base_locations_api.permissions import IsSuperUserOrReadOnly
+from .models import Location
+from .serializers import LocationSerializer
 
-# Create your views here.
+class LocationList(generics.ListCreateAPIView):
+    """
+    List all locations or create a new location if superuser.
+    """
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [IsSuperUserOrReadOnly]
+
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a location. Only superusers can modify.
+    """
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [IsSuperUserOrReadOnly]
