1d6db5551087080586016263e71ea5626a30a9f9
Jxkeorton <jakeorton99@gmail.com>
2024-11-06 22:45:29 +0000

    Allow null fields

--- locations/models.py
+++ locations/models.py
@@ -8,13 +8,13 @@ class Location(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     rock_drop = models.IntegerField()
-    total_height = models.IntegerField()
+    total_height = models.IntegerField(null=True, blank=True),
     access = models.CharField(max_length=250)
     cliff_aspect = models.CharField(max_length=3)
     opened_by = models.CharField(max_length=100, null=True, blank=True)
     date_opened = models.DateField(null=True, blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_qpihoy', blank=True
+        upload_to='images/', default='../default_post_qpihoy', blank=True, null=True
     )
 
     def __str__(self):
