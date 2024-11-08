4ba8169eaefb479e0ea0831b2cf7fef8b58ed26b
Jxkeorton <jakeorton99@gmail.com>
2024-11-08 10:40:36 +0000

    Add saved locations views

--- saved_locations/views.py
+++ saved_locations/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from .models import SavedLocation
+from .serializers import SavedLocationSerializer
 
-# Create your views here.
+class SavedLocationList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = SavedLocationSerializer
+
+    def get_queryset(self):
+        return SavedLocation.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class SavedLocationDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = SavedLocationSerializer
+    queryset = SavedLocation.objects.all()
+
+    def get_queryset(self):
+        return SavedLocation.objects.filter(owner=self.request.user)
