1448ef58eb54d4ff3d65d0e87b9766f4dc104eb6
Jxkeorton <jakeorton99@gmail.com>
2024-11-09 17:31:47 +0000

    Update saved locations

--- saved_locations/serializers.py
+++ saved_locations/serializers.py
@@ -3,7 +3,16 @@ from .models import SavedLocation
 
 class SavedLocationSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-
+    location_name = serializers.ReadOnlyField(source='location.name')
+    location_image = serializers.ReadOnlyField(source='location.image')
+    
     class Meta:
         model = SavedLocation
-        fields = ['id', 'owner', 'location', 'created_at']
+        fields = [
+            'id', 
+            'owner', 
+            'location',
+            'location_name',
+            'location_image', 
+            'created_at'
+        ]
