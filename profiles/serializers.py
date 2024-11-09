09517a422d3934faa03deefaecdf8e37fe77f78c
Jxkeorton <jakeorton99@gmail.com>
2024-11-09 14:16:59 +0000

    Update to make image optional

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from .models import Profile
 
-
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField() 
+    is_owner = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def update(self, instance, validated_data):
+        if 'image' in validated_data and not validated_data['image']:
+            validated_data.pop('image')
+        return super().update(instance, validated_data)
 
     class Meta:
         model = Profile
