4710504a31cf012236743bb0a89b07f7b42da7ac
Jxkeorton <jakeorton99@gmail.com>
2024-12-18 18:08:39 +0000

    Add maxLengthValidators to models

--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=255, blank=True)
-    no_of_base_jumps = models.IntegerField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True)
+    no_of_base_jumps = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0)])
     image = models.ImageField(
         upload_to='images/', default='../default_profile_drmlhx'
     )
--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator
 
 class Review(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     location = models.ForeignKey('locations.Location', on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(validators=[MaxLengthValidator(500)])
     hazard = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
